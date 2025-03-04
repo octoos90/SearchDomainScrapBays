5778260774854de7b8c6f9a6a75288f000bc46d8
Octo Siswardhono <siswardhono@gmail.com>
2025-03-04 21:02:26 +0700

    initialize SwiftUI project

---
+++ SearchDomainScrapBays/SearchDomainScrapBays/SearchDomainScrapBaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchDomainScrapBaysApp.swift
+//  SearchDomainScrapBays
+//
+//  Created by Octo Siswardhono on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchDomainScrapBaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SearchDomainScrapBays/SearchDomainScrapBaysUITests/SearchDomainScrapBaysUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SearchDomainScrapBaysUITestsLaunchTests.swift
+//  SearchDomainScrapBaysUITests
+//
+//  Created by Octo Siswardhono on 04/03/25.
+//
+
+import XCTest
+
+final class SearchDomainScrapBaysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
